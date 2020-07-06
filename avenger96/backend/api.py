04bd41437b2aa0fa333bf7804917a7c09c000c31
Feromrk <johannes.stark.js@gmail.com>
2020-07-06 02:42:04 +0200

    added firmware update feature

--- avenger96/backend/api.py
+++ avenger96/backend/api.py
@@ -1,18 +1,24 @@
 import csv
 import json
+import os
 
 import flask
-from flask import request, Response
+from flask import request, Response, send_file
+
 import cerberus
+
+import glob
+import natsort
 
 
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
 
 db_filename = 'db.csv'
+firmware_folder = '/var/opt/dodo/sensor_firmware'
 
-@app.route('/update-sensor', methods=['GET'])
-def get_sensor_update():
+@app.route('/sensor-push', methods=['GET'])
+def get_sensor_push():
     request.parameter_storage_class = dict
 
     schema = {
@@ -34,15 +40,6 @@ def get_sensor_update():
             'required': True,
             'coerce': float
         },
-        'hum_in': {
-            'type': 'float',
-            'anyof': [
-                {'min': 0, 'max': 100},
-                {'allowed': [-1]}
-            ],
-            'required': True,
-            'coerce': float
-        },
         'rpi_state': {
             'type': 'integer',
             'coerce': int,
@@ -58,6 +55,11 @@ def get_sensor_update():
             'type': 'integer',
             'coerce': int,
             'required': True,
+        },
+        'fw_version': {
+            'type': 'integer',
+            'required': True,
+            'coerce': int
         }
     }
 
@@ -72,9 +74,9 @@ def get_sensor_update():
             'timestamp', 
             'temp_in',
             'temp_out',
-            'hum_in',
             'rpi_state',
-            'bat'
+            'bat',
+            'fw_version'
         ]
         writer = csv.DictWriter(f, fieldnames=fieldnames, extrasaction='ignore')
 
@@ -114,5 +116,37 @@ def get_sensor_data():
         resp = Response(json.dumps(r_dict), status=200, content_type='application/json')
         resp.headers['Access-Control-Allow-Origin'] = '*'
         return resp
+
+
+@app.route('/sensor-firmware', methods=['GET'])
+def get_sensor_firmware():
+
+    print(request.headers)
+
+    user_agent = request.headers.get('User-Agent')
+    if not user_agent == 'ESP8266-http-Update':
+        print('Wrong user agent!', user_agent)
+        return Response('Only for ESP8266 updater!',status=403, content_type='text/plain')
+
+    firmware_filenames = glob.glob(f"{firmware_folder}/*.bin")
+
+    if firmware_filenames:
+        latest_firmware_filename = natsort.natsorted(firmware_filenames, reverse=True)[0]
+        latest_firmware_version = os.path.basename(latest_firmware_filename).split('.')[0]
+
+    remote_firmware_version = request.headers.get('X-Esp8266-Version')
+
+    try:
+        remote_firmware_version = int(request.headers['X-Esp8266-Version'])
+    except (KeyError, ValueError) as e:
+        print(e)
+        return Response('X-Esp8266-Version header missing', status=403, content_type='text/plain')
+
+    if int(latest_firmware_version) > remote_firmware_version:
+        return send_file(latest_firmware_filename, as_attachment=True)
+    
+    return Response(status=304)
+    
+
 app.run(host='192.168.2.117', port=5000)
 #app.run(host='0.0.0.0', port=5000)
--- avenger96/backend/plot.py
+++ avenger96/backend/plot.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as md
 import datetime as dt
 
-data = np.genfromtxt('db.csv', delimiter=',', names=['x', 'y'], usecols=np.arange(0,4))
+data = np.genfromtxt('db.csv', delimiter=',', names=['timestamps', 'temp_in', 'temp_out'], usecols=np.arange(0,4))
 
 
 fig = plt.figure()
@@ -19,9 +19,11 @@ ax1.set_title("Temperaturen im Hühnerhaus")
 ax1.set_xlabel('Zeit')
 ax1.set_ylabel('Celsius')
 
-x = [dt.datetime.fromtimestamp(ts) for ts in data['x']]
+x = [dt.datetime.fromtimestamp(ts) for ts in data['timestamps']]
+temp_out = [(temp_out if temp_out > 0 else None) for temp_out in data['temp_out']]
 
-ax1.plot(x,data['y'], c='r', label='innen')
+ax1.plot(x,data['temp_in'], c='r', label='innen')
+ax1.plot(x,temp_out, c='b', label='außen')
 
 leg = ax1.legend()
 
